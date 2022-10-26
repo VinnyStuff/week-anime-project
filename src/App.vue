20784d814a42c519cefb14f2c0cd39aeed8e412c
DESKTOP-0OFJ21K\Work <vinnyciosdev@gmail.com>
2022-10-26 11:54:00 -0300

    learning vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AnimeDaysOfWeekVue from './components/AnimeDaysOfWeek.vue';
+</script>
+
+<template>
+  <div>
+      <AnimeDaysOfWeekVue/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AnimeDaysOfWeek.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="AnimeDaysOfWeek">
+        <h1>Hello World</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "AnimeDaysOfWeek"
+};
+</script>
+
+<style scoped>
+
+</style>
